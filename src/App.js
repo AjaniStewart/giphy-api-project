4663d5170c733de1e76e6aaef9a68a4f089154f3
Toms <chen.tommy925@gmail.com>
2019-07-25 13:51:39 -0400

    search field

--- src/App.js
+++ src/App.js
@@ -5,12 +5,19 @@ import GifCard from "./components/GifCard/GifCard";
 import './App.css';
 
 class App extends Component {
-  return () {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+  render () {
     return (
         <div className="App">
           <header className="App-header">
-        
+              <h2>Giphy</h2>
           </header>
+          <SearchField/>
         </div>
     );
   }
--- src/components/SearchField/SearchField.js
+++ src/components/SearchField/SearchField.js
@@ -1,2 +1,48 @@
 import React, {Component} from 'react';
+import GifCard from "../GifCard/GifCard";
+import axios from 'axios';
 import "./SearchField.css"
+
+const api_key = 'LgtNkTTMlSnTMNsRZvRxkY4og7ExPyhM';
+
+class SearchField extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: "",
+            
+            fetched: true
+            
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+handleChange = event => {
+    console.log(event.target.value);
+    this.setState({term: event.target.value});
+}
+handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state.term);
+    this.searchGif();
+}
+
+    render () {
+        
+        return (
+            <div className = "search-field">
+                <form>
+                    <label>
+                        Search:
+                        <input class = "input" type = "text" placeholder = "Doge" onChange = {this.handleChange}/>
+                    </label>
+                    <button class = "button" onClick={this.handleSubmit}>Submit</button>
+                </form>
+                
+            </div>
+        );
+    }
+}
+
+export default SearchField;
